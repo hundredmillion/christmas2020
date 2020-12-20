1449526736f66f02cbb2bdbcfc90d3ce11f31a52
hundredmillion <hundredmillion@pm.me>
2020-12-20 10:58:41 +0000

    styling updates

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,6 @@ const present = document.getElementById("present");
 const ribbon = document.getElementById("ribbon");
 
 presentBox.addEventListener("click", function () {
-  present.classList.toggle("d-none");
-  ribbon.classList.toggle("d-none");
+  present.classList.remove("d-none");
+  ribbon.classList.add("d-none");
 });
